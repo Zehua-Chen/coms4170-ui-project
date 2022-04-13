d891c1ce10a9f87b0232bc420c96fd4443e60592
Zehua-Chen <zehua-chen@outlook.com>
2022-04-13 00:23:27 -0400

    [feat] Create Konva Stage

--- src/components/otamatone.ts
+++ src/components/otamatone.ts
@@ -1,47 +1,99 @@
 import { createComponentFromClass, ClassComponent } from "./component";
+import Konva from "konva";
 
-type OtamatoneLabels = { [position in number]: string };
-type OtamatoneAudios = { [position in number]: string };
+type Position = number;
 
 export interface OtamatoneConfiguration {
   /**
-   * Map a position in the otamatone a string label
+   * The enabled positions
    */
-  labels?: OtamatoneLabels;
+  positions: Position[];
 
   /**
-   * Map a position in the otamatone to a audio file
+   * How to display labels of enabled positions
    */
-  audios?: OtamatoneAudios;
+  labels: "string" | "number";
 
   /**
    * Called when a position is played
    */
-  onPlay?: (position: number) => any;
+  onPlay: (position: Position) => any;
 }
 
-export class OtamatoneComponent extends ClassComponent<OtamatoneConfiguration> {
-  private counter: number = 0;
+function applyDefaultConfiguration(
+  configuration: Partial<OtamatoneConfiguration>
+): OtamatoneConfiguration {
+  const {
+    positions = [],
+    labels = "string",
+    onPlay = () => {},
+  } = configuration;
 
-  constructor(root: JQuery, config: OtamatoneConfiguration) {
+  return {
+    positions,
+    labels,
+    onPlay,
+  };
+}
+
+export class OtamatoneComponent extends ClassComponent<
+  Partial<OtamatoneConfiguration>,
+  HTMLDivElement
+> {
+  private counter: number = 0;
+  private observer: ResizeObserver;
+  private stage: Konva.Stage;
+
+  constructor(
+    root: JQuery<HTMLDivElement>,
+    config: Partial<OtamatoneConfiguration>
+  ) {
     super(root, config);
+
+    const solidConfig = applyDefaultConfiguration(config);
 
     root.empty();
 
-    root.text("placeholder for otamatone...");
+    const rootElement = root.get()[0];
+
+    this.observer = new ResizeObserver(this.onResize.bind(this));
+    this.observer.observe(rootElement);
+
+    this.stage = new Konva.Stage({
+      container: rootElement,
+      height: 200,
+      width: rootElement.clientWidth,
+    });
+    const layer = new Konva.Layer({});
+    this.stage.add(layer);
+
+    const rect1 = new Konva.Rect({
+      x: 20,
+      y: 20,
+      width: 400,
+      height: 20,
+      fill: "white",
+      stroke: "black",
+      strokeWidth: 2,
+    });
+    // add the shape to the layer
+    layer.add(rect1);
   }
 
   public override update(
     root: JQuery<HTMLElement>,
-    config: OtamatoneConfiguration
+    config: Partial<OtamatoneConfiguration>
   ): void {
     this.counter += 1;
 
     console.log(`update ${this.counter}`);
   }
+
+  private onResize(entries: ResizeObserverEntry[]) {
+    const entry = entries[0];
+
+    this.stage.width(entry.contentRect.width);
+  }
 }
 
-export default createComponentFromClass<
-  OtamatoneConfiguration,
-  OtamatoneComponent
->(OtamatoneComponent);
+export default createComponentFromClass(OtamatoneComponent);
