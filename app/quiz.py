d1bd784db7c092bfe8b5e2efd601ea08da793544
Zehua-Chen <zehua-chen@outlook.com>
2022-04-19 14:54:59 -0400

    [fix] Fix Syntax Errors in Quiz

--- app/quiz.py
+++ app/quiz.py
@@ -3,7 +3,7 @@ from typing import Dict, TypedDict, List
 
 class Quiz(TypedDict):
     id: int
-    title:string
+    title: str
     notes: list
     positions_to_click: list
 
@@ -11,44 +11,44 @@ class Quiz(TypedDict):
 quizzes = {
     1: {
         "id": 1,
-        "title":"quiz1"
+        "title": "quiz1",
         "notes": [],
         "positions_to_click": []
     },
     2: {
         "id": 2,
-        "title":"quiz2"
+        "title": "quiz2",
         "notes": [],
         "positions_to_click": []
     },
     3: {
         "id": 3,
-        "title":"quiz3"
+        "title": "quiz3",
         "notes": [],
         "positions_to_click": []
     },
     4: {
         "id": 4,
-        "title":"quiz4"
+        "title": "quiz4",
         "notes": [],
         "positions_to_click": []
     },
     5: {
         "id": 5,
-        "title":"quiz5"
+        "title": "quiz5",
         "notes": [],
         "positions_to_click": []
     },
     6: {
         "id": 6,
-        "title":"quiz6"
+        "title": "quiz6",
         "notes": [],
         "positions_to_click": []
     }
-}  # type: Dict[int, quiz]
+}  # type: Dict[int, Quiz]
 
 quizzes_overview = list(map(
-    lambda pair: pair[1]["note"],
+    lambda pair: pair[1]["title"],
     # sort lessons by lesson id
     sorted(
         quizzes.items(), key=lambda x: x[0])))  # type: List[str]
