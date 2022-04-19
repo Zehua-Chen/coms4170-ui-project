815191d7d1d8ea37bf6d0ccc668b0b811d239477
sl4986 <sl4986@columbia.edu>
2022-04-19 18:41:01 +0000

    this is a test

---
+++ src/pages/test.ts
@@ -0,0 +1,64 @@
+import navbar from "../components/navbar";
+import otamatone from "../components/otamatone";
+import sidebar from "../components/sidebar";
+
+function userInputs(inputs: number[]) {
+  $("#user-inputs")
+    .empty()
+    .append(inputs.map((input) => $("<span />").text(input)));
+}
+
+function practice(): void {
+  navbar({ active: "Practice" });
+  const { data } = window.App;
+
+  if (!data) {
+    throw Error("window.App.data should not be undefined");
+  }
+
+  if (data.type !== "practice") {
+    throw Error(`expect data type to be 'learn', got ${data.type}`);
+  }
+
+  const { practice, practices_overview } = data;
+  const { id, positions_to_click } = practice;
+
+  const nextID = id + 1;
+  const positionsClicked: number[] = [];
+
+  $("#next").on("click", () => {
+    if (nextID > practices_overview.length) {
+      window.location.href = "/finish";
+    } else {
+      window.location.href = `/quiz/${nextID}`;
+    }
+  });
+
+  $("#clip")
+    .empty()
+    .append(
+      $("<audio controls />").append(
+        $("<source />")
+          .attr("src", `/practice/clip/${id}.mp3`)
+          .attr("type", "audio/mpeg")
+      )
+    );
+
+  otamatone($("#otamatone"), {
+    onPlay(position) {
+      positionsClicked.push(position);
+
+      userInputs(positionsClicked);
+
+      if (
+        JSON.stringify(positionsClicked) === JSON.stringify(positions_to_click)
+      ) {
+        $("#next").removeAttr("disabled");
+      }
+    },
+  });
+
+  sidebar({ elements: practices_overview, active: practice.title });
+}
+
+export default practice;
