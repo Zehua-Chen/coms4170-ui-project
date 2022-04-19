d179f2d17b8e5503cdb07cde2114d1513294b3df
nuomicii <yj2680@columbia.edu>
2022-04-19 13:57:07 -0400

    data? for quiz

---
+++ app/quiz.py
@@ -0,0 +1,54 @@
+from typing import Dict, TypedDict, List
+
+
+class Quiz(TypedDict):
+    id: int
+    title:string
+    notes: list
+    positions_to_click: list
+
+
+quizzes = {
+    1: {
+        "id": 1,
+        "title":"quiz1"
+        "notes": [],
+        "positions_to_click": []
+    },
+    2: {
+        "id": 2,
+        "title":"quiz2"
+        "notes": [],
+        "positions_to_click": []
+    },
+    3: {
+        "id": 3,
+        "title":"quiz3"
+        "notes": [],
+        "positions_to_click": []
+    },
+    4: {
+        "id": 4,
+        "title":"quiz4"
+        "notes": [],
+        "positions_to_click": []
+    },
+    5: {
+        "id": 5,
+        "title":"quiz5"
+        "notes": [],
+        "positions_to_click": []
+    },
+    6: {
+        "id": 6,
+        "title":"quiz6"
+        "notes": [],
+        "positions_to_click": []
+    }
+}  # type: Dict[int, quiz]
+
+quizzes_overview = list(map(
+    lambda pair: pair[1]["note"],
+    # sort lessons by lesson id
+    sorted(
+        quizzes.items(), key=lambda x: x[0])))  # type: List[str]
