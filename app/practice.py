a62edc881ae7a2ee4844bc2d8e6e03f21012d9ce
nuomicii <yj2680@columbia.edu>
2022-04-23 18:54:06 -0400

    practice

--- app/practice.py
+++ app/practice.py
@@ -15,7 +15,7 @@ practices = {
     1: {
         "id": 1,
         "title": "Practice 1",
-        "positions_to_click": [1, 2, 3]
+        "positions_to_click": [1,1,2,2,3,3]
     },
     2: {
             "id": 2,
