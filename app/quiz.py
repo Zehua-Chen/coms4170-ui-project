cce2958b033af60cacb3d2a83d82a11337e70c87
Zehua-Chen <zehua-chen@outlook.com>
2022-04-23 17:44:55 -0400

    [fix] Fix Python Type Checking Error

--- app/quiz.py
+++ app/quiz.py
@@ -7,6 +7,7 @@ from .view import render_template
 class Quiz(TypedDict):
     id: int
     title: str
+    subtitle: str
     notes: list
     positions_to_click: list
 
@@ -20,39 +21,42 @@ quizzes = {
     1: {
         "id": 1,
         "title": "Question 1",
-        "subtitle":"Find the note you hear",
+        "subtitle": "Find the note you hear",
         "notes": [],
         "positions_to_click": []
     },
     2: {
         "id": 2,
         "title": "Question 2",
-        "subtitle":"Which two notes were played",
+        "subtitle": "Which two notes were played",
         "notes": [],
         "positions_to_click": []
     },
     3: {
         "id": 3,
         "title": "Question 3",
-        "subtitle":"Play the three notes you hear in sequence",
+        "subtitle": "Play the three notes you hear in sequence",
         "notes": [],
         "positions_to_click": []
     },
     4: {
         "id": 4,
         "title": "quiz4",
+        "subtitle": "",
         "notes": [],
         "positions_to_click": []
     },
     5: {
         "id": 5,
         "title": "quiz5",
+        "subtitle": "",
         "notes": [],
         "positions_to_click": []
     },
     6: {
         "id": 6,
         "title": "quiz6",
+        "subtitle": "",
         "notes": [],
         "positions_to_click": []
     }
@@ -65,11 +69,11 @@ quiz_solutions = {
     },
     2: {
         "id": 2,
-        "solution": [1,2]
+        "solution": [1, 2]
     },
     3: {
         "id": 3,
-        "solution": [3,4,5]
+        "solution": [3, 4, 5]
     },
     4: {
         "id": 4,
