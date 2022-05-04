6141143f015276bfa15f91b5f965e516161c430c
nuomicii <yj2680@columbia.edu>
2022-05-03 21:16:27 -0400

    add practice description

--- app/practice.py
+++ app/practice.py
@@ -10,23 +10,27 @@ class Practice(TypedDict):
     id: int
     title: str
     positions_to_click: List[int]
+    description:str
 
 
 practices = {
     1: {
         "id": 1,
         "title": "Practice 1",
-        "positions_to_click": [1, 1, 2, 2, 3, 3]
+        "positions_to_click": [1, 1, 2, 2, 3, 3],
+        "description":"Try to replicate the clip with three notes"
     },
     2: {
         "id": 2,
         "title": "Practice 2",
-        "positions_to_click": [1, 1, 5, 5, 6, 6, 5]
+        "positions_to_click": [1, 1, 5, 5, 6, 6, 5],
+        "description":"Try to replicate the clip with three notes"
     },
     3: {
         "id": 3,
         "title": "Practice 3",
-        "positions_to_click": [3, 6, 5, 6, 5, 2]
+        "positions_to_click": [3, 6, 5, 6, 5, 2],
+        "description":"Try to replicate the clip with four notes"
     }
 }  # type: Dict[int, Practice]
 
