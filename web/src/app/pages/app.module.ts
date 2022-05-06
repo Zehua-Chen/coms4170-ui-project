2a2fb91ee200344a7474221477434edb73a4cc4e
Zehua-Chen <zehua-chen@outlook.com>
2022-05-06 16:52:14 -0400

    [refactor] Restructure Project

--- web/src/app/pages/app.module.ts
+++ web/src/app/pages/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { OtamatoneModule } from 'components/otamatone';
-import { LearnComponent } from './learn.component';
+import { LearnComponent } from './learn/learn.component';
 
-const routes = [{ path: '', component: LearnComponent }] as Routes;
+const routes = [{ path: 'learn', component: LearnComponent }] as Routes;
 
 @NgModule({
   declarations: [LearnComponent],
   imports: [RouterModule.forChild(routes), CommonModule, OtamatoneModule],
   exports: [RouterModule],
 })
-export class LearnModule {}
+export class AppModule {}
---
+++ web/src/app/root.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+
+import { RootComponent } from './root.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { OtamatoneModule } from 'components/otamatone';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'app',
+    async loadChildren() {
+      const { AppModule } = await import('./pages/app.module');
+      return AppModule;
+    },
+  },
+];
+
+@NgModule({
+  declarations: [RootComponent, WelcomeComponent],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(routes),
+    MatButtonModule,
+    MatToolbarModule,
+    OtamatoneModule,
+  ],
+  providers: [],
+  bootstrap: [RootComponent],
+})
+export class RootModule {}
