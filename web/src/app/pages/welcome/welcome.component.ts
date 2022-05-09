4c114c0828ae18e9d8fa51a5320852cc1384fd15
Zehua-Chen <zehua-chen@outlook.com>
2022-05-09 13:26:27 -0400

    [feat] Integrate Tailwind

--- web/src/app/pages/welcome/welcome.component.ts
+++ web/src/app/pages/welcome/welcome.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent {
   positions: number[] = [1, 2, 3];
