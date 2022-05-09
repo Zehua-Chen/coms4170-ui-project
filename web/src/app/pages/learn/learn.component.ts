7edf01f83d083674ab26fe055433646e56c03909
Zehua-Chen <zehua-chen@outlook.com>
2022-05-09 19:03:32 -0400

    [feat] Add Dynamic Content for Previous and Next

--- web/src/app/pages/learn/learn.component.ts
+++ web/src/app/pages/learn/learn.component.ts
@@ -1,4 +1,4 @@
-import { Observable, map, zip } from 'rxjs';
+import { Observable, map, zip, of } from 'rxjs';
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LessonService, Lesson, LessonOverview } from 'app/api';
@@ -19,13 +19,38 @@ export class LearnPage implements OnInit {
 
   lesson!: Observable<Lesson>;
   lessonOverviews!: Observable<LessonOverview[]>;
+  finishedLessons!: Observable<[Lesson, LessonOverview[]]>;
 
-  get previousLessonLink(): Observable<string> {
+  get previousDisabled(): Observable<boolean> {
+    return this.finishedLessons.pipe(
+      map(([lesson, lessonOverviews]) => {
+        if (lesson.id === lessonOverviews[0].id) {
+          return true;
+        }
+
+        return false;
+      })
+    );
+  }
+
+  get previousLink(): Observable<string> {
     return this.lesson.pipe(map((lesson) => `/app/learn/${lesson.id - 1}`));
   }
 
-  get nextLessonLink(): Observable<string> {
-    return zip(this.lesson, this.lessonOverviews).pipe(
+  get nextText(): Observable<string> {
+    return this.finishedLessons.pipe(
+      map(([lesson, lessonOverviews]) => {
+        if (lesson.id === lessonOverviews[lessonOverviews.length - 1].id) {
+          return 'Practice';
+        }
+
+        return `Next`;
+      })
+    );
+  }
+
+  get nextLink(): Observable<string> {
+    return this.finishedLessons.pipe(
       map(([lesson, lessonOverviews]) => {
         if (lesson.id === lessonOverviews[lessonOverviews.length - 1].id) {
           return '/app/practice';
@@ -49,6 +74,7 @@ export class LearnPage implements OnInit {
 
       this.lessonOverviews = this.lessonService.getLessonOverviews();
       this.lesson = this.lessonService.getLesson(id);
+      this.finishedLessons = zip(this.lesson, this.lessonOverviews);
     });
   }
 
