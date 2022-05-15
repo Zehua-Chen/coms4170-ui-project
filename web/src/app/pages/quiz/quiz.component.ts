789e4b269493b81220601b3ccc61fba6652d31dc
Zehua-Chen <zehua-chen@outlook.com>
2022-05-14 23:04:48 -0400

    [feat] Calculate Total Score

--- web/src/app/pages/quiz/quiz.component.ts
+++ web/src/app/pages/quiz/quiz.component.ts
@@ -2,16 +2,25 @@ import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import {
   combineLatest,
   map,
+  first,
   filter,
   mergeMap,
   Observable,
   Subscription,
 } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { FormControl } from '@angular/forms';
 
-import { Quiz, Question, QuizService } from 'api/quiz.service';
+import {
+  Quiz,
+  Question,
+  QuizService,
+  quizTotalScore,
+  quizScore,
+} from 'api/quiz.service';
 import { OtamatoneClip } from 'components/otamatone-clip';
+import { isFirst, isLast } from 'utils/rxjs';
+
+export type QuestionAndIndex = { question: Question; index: number };
 
 @Component({
   selector: 'app-quiz',
@@ -27,9 +36,14 @@ export class QuizPage implements OnInit, OnDestroy {
     mergeMap((id) => this.quizService.getQuiz(id!))
   );
 
-  questions$: Observable<Question[]> = this.quiz$.pipe(
+  quizTotalScore$: Observable<number> = this.quiz$.pipe(quizTotalScore());
+  quizScore$: Observable<number> = this.quiz$.pipe(quizScore());
+
+  questions$: Observable<QuestionAndIndex[]> = this.quiz$.pipe(
     filter((quiz) => quiz !== undefined),
-    map((quiz) => quiz!.questions)
+    map((quiz) =>
+      quiz!.questions.map((question, index) => ({ question, index }))
+    )
   );
 
   questionIndex$: Observable<number> = this.route.paramMap.pipe(
@@ -38,10 +52,14 @@ export class QuizPage implements OnInit, OnDestroy {
     map((question) => Number.parseInt(question))
   );
 
-  question$: Observable<Question | undefined> = combineLatest([
+  question$: Observable<QuestionAndIndex> = combineLatest([
     this.questions$,
     this.questionIndex$,
   ]).pipe(map(([questions, index]) => questions[index]));
+
+  previousDisabled$: Observable<boolean>;
+
+  nextDisabled$: Observable<boolean>;
 
   /**
    * Notes to be played must be set in TypeScript, otherwise notes will be
@@ -53,22 +71,48 @@ export class QuizPage implements OnInit, OnDestroy {
   notes: number[] = [];
 
   clipNoteSubscription: Subscription = this.question$.subscribe((question) => {
-    this.clip.notes = question?.solution ?? [];
+    this.clip.notes = question?.question?.solution ?? [];
   });
 
   notesSubscription: Subscription = this.question$.subscribe((question) => {
-    this.notes = question?.submission ?? [];
+    this.notes = question?.question?.submission ?? [];
   });
 
-  constructor(
-    private route: ActivatedRoute,
-    private quizService: QuizService
-  ) {}
+  constructor(private route: ActivatedRoute, private quizService: QuizService) {
+    const isFirstQuestion: Observable<[QuestionAndIndex | null, boolean]> =
+      combineLatest([this.question$, this.questions$]).pipe(isFirst());
+
+    const isLastQuestion: Observable<[QuestionAndIndex | null, boolean]> =
+      combineLatest([this.question$, this.questions$]).pipe(isLast());
+
+    this.previousDisabled$ = isFirstQuestion.pipe(
+      map(([_, first]) => {
+        return first;
+      })
+    );
+
+    this.nextDisabled$ = isLastQuestion.pipe(
+      map(([_, last]) => {
+        return last;
+      })
+    );
+  }
 
   ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.clipNoteSubscription.unsubscribe();
     this.notesSubscription.unsubscribe();
+  }
+
+  submit(): void {
+    this.question$
+      .pipe(
+        map((question) => question.question),
+        first()
+      )
+      .subscribe((question) => {
+        console.log(question);
+      });
   }
 }
